8ed53fbeed0fda71d021173a61640d2f0d869af2
Anusha <anushavysakh232@gmail.com>
2022-07-29 07:35:35 +0530

    Uc6 committed

--- AddressBook.java
+++ AddressBook.java
@@ -1,14 +1,15 @@
 package com.addressbook.bridglabz;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class Uc5 {
+public class Uc6 {
 
-	ArrayList<ArrayList<String>> contact = new ArrayList<ArrayList<String>>();
-	Scanner sc = new Scanner(System.in);
+	  static HashMap<String, ArrayList> books = new HashMap<>();
+	  Scanner sc = new Scanner(System.in);
 
-	public void contactInfo() {
+	public void contactInfo(ArrayList<ArrayList<String>> contact) {
 		ArrayList<String> addInfo = new ArrayList<String>();
 		System.out.println("Enter First name : ");
 		addInfo.add(sc.next());
@@ -26,22 +27,27 @@ public class Uc5 {
 		addInfo.add(sc.next());
 		System.out.println("Enter Contact number : ");
 		addInfo.add(sc.next());
+		
 		contact.add(addInfo);
 		System.out.println("The contact has been added to the Address Book");
-		for (int i = 0; i < contact.size(); i++) {
-			for (int j = 0; j < contact.get(i).size(); j++) {
-				System.out.print(contact.get(i).get(j) + " ");
-			}
-		}
 		System.out.println("Do you want to add another contact to the Address Book? (Y/N)");
 		String choice = sc.next();
 		if (choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("yes")) {
-			contactInfo();
+			contactInfo(contact);
 		} else {
 			userChoice();
 		}
 	}
-		 public void editContact() {
+	
+	  public void displayContact(ArrayList<ArrayList<String>> contact) {
+	        for(int i = 0; i < contact.size(); i++) {
+	            for (int j = 0; j < contact.get(i).size(); j++) {
+	                System.out.print(contact.get(i).get(j) + " ");
+	            }
+	            System.out.println();
+	        }
+	    }
+		 public void editContact(ArrayList<ArrayList<String>> contact) {
 		        System.out.print("Please enter the First Name of the person you want to edit the contact of: ");
 		        String name = sc.next();
 		        for(int i = 0; i < contact.size(); i++){
@@ -63,32 +69,49 @@ public class Uc5 {
 		    }
 
 
-		public void userChoice() {
+		  public ArrayList<ArrayList<String>> userChoice() {
+		        ArrayList<ArrayList<String>> contact = new ArrayList<ArrayList<String>>();
+		        System.out.println("Current Records present in the Address Book are " + contact.size());
+		        System.out.println("Please select your choice, do you want to\n " +
+		                            "0) Exit the Program\n" +
+		                            "1) Add New Contact\n" +
+		                            "2) Edit Existing Contact\n" +
+		                            "3) Delete a Contact\n" +
+		                            "4) Display All Contacts\n" +
+		                            "5) Add New Address Book");
+		        int choice = sc.nextInt();
+		        switch (choice) {
+		            case 0:
+		                return contact;
+		            case 1:
+		                contactInfo(contact);
+		                return contact;
 
-			int ch;
-			System.out.println("Enter your choice\n 1.Add contact\n 2.Edit contact\n3.Delete contact");
-			ch = sc.nextInt();
-			switch (ch) {
-			case 0:
-				System.exit(0);
-				break;
-			case 1:
-				contactInfo();
-				break;
-			case 2:
-				editContact();
-				break;
-			case 3:
-				deleteContact();
-				break;
-			default:
-				System.out.println("Enter the valid choice:");
-				userChoice();
-				break;
-			}
-		}
+		            case 2:
+		                editContact(contact);
+		                return contact;
 
-		public void deleteContact() {
+		            case 3:
+		                deleteContact(contact);
+		                return contact;
+
+		            case 4:
+		                displayContact(contact);
+		                return contact;
+
+		            case 5:
+		                addBooks();
+		                return contact;
+
+		            default:
+		                System.out.println("Please enter the Number associated with the choice");
+		                userChoice();
+		                break;
+		        }
+		        return contact;
+		    }
+
+		public void deleteContact(ArrayList<ArrayList<String>> contact) {
 			System.out.println("Enter the first name of the contact you need to delete :");
 			String name = sc.next();
 
@@ -101,13 +124,21 @@ public class Uc5 {
 	                }
 			}
 		}
+		
+		public void addBooks() {
+	        System.out.println("Current Number of Address Books in the Database are: " + books.size());
+	        System.out.print("Enter the Address Books Name: ");
+	        books.put(sc.next(), userChoice());
+		}
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-			Uc5 obj = new Uc5();
+			Uc6 obj = new Uc6();
 			// obj.contactInfo();
 			// obj.addNew();
 			obj.userChoice();
+			obj.addBooks();
+			
 		}
 
 }
